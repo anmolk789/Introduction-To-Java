f1dfe9a7f728310b47be62053d5e5380eb2319e8
Anmol Kumar <kumaanm@gmail.com>
2020-04-01 12:22:15 +0530

    Add files via upload

---
+++ Welcoming Freshmen/Welcoming Freshmen.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main (String[] args) {
+        String s;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the name");
+        s=obj.nextLine();
+        System.out.println("Welcome "+s+"! Achieve with all your might.");
+    }
+}
